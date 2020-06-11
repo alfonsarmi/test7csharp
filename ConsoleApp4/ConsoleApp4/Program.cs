d84b4ccf2663850b891250219e1c3cf477ded430
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-06-11 11:57:27 +0200

    FirstCommit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			var a = 1;
+			var b = 1;
+			if (a == 1) 
+			{
+				if (a == 1 && b == 2 || b == 1) 
+				{
+					if (b == 2)  
+					{
+						switch (b) 
+						{
+							case 1:
+							break;
+				
+							case 2:
+							break;
+
+							default:
+							break;
+
+						}
+
+					}
+				}
+
+			}
+		}
+    }
+}
